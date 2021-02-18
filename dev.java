0f5ffe37359e1c8b1dbff6b150a9cea7610fcf00
yogi <yogi@gmail.com>
2021-02-18 11:33:28 +0000

    Added dev.java

---
+++ dev.java
@@ -0,0 +1,15 @@
+public class addition
+{
+	public void feature1()
+	{
+		int a,b,c;
+		a=10;
+		b=20;
+		c=a+b;}
+
+	public static void main(String args[])
+	{
+		addition a=new addition();
+		a.feature1();
+	}
+}
